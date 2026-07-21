694e0a055ec355931de5ac6020b7623a95f8ce0e
gavravvv17 <gavravh05@gmail.com>
2026-07-21 17:21:02 +0530

    Added Student Model Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import model.Student;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Student Grade Tracker!");
+
+        int[] marks = {85, 90, 78, 92, 88};
+
+        Student student = new Student(
+                "Gaurav",
+                "101",
+                marks
+        );
+
+        System.out.println("Name : " + student.getName());
+        System.out.println("Roll : " + student.getRollNo());
+
+        System.out.print("Marks : ");
+
+        for (int mark : student.getMarks()) {
+            System.out.print(mark + " ");
+        }
     }
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,38 @@
 package model;
 
 public class Student {
-    
+
+    private String name;
+    private String rollNo;
+    private int[] marks;
+
+    public Student(String name, String rollNo, int[] marks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public int[] getMarks() {
+        return marks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public void setMarks(int[] marks) {
+        this.marks = marks;
+    }
 }
